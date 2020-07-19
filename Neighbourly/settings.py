e1441f93488c7c9e1118a385e015295616696a04
Rose Kairu <rosekairu@gmail.com>
2020-07-19 19:59:46 +0300

    configured cloudinarly

--- Neighbourly/settings.py
+++ Neighbourly/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'Posts',
     'bootstrap4',
+    'phone_field',
     'cloudinary',
     'rest_framework',
     'rest_framework.authtoken'
@@ -133,7 +134,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
