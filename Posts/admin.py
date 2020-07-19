6daf7c888a342724937805447f53b9fdcb246d10
Rose Kairu <rosekairu@gmail.com>
2020-07-19 21:37:34 +0300

    registered models in admin

--- Posts/admin.py
+++ Posts/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
 from django.contrib.auth.models import User
-from .models import *
-
+from .models import Neighborhood,Profile,Business,Notice,HealthCenter,Police
 #registering the models
 admin.site.register(Neighborhood)
 admin.site.register(Profile)
